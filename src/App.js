23ba38fbc6694ab3d6d78e04e5f3c487fff5d15c
samuel <samueldcabral@gmail.com>
2018-11-15 20:43:18 -0300

    Creating To do List App and setting a simple delete action

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos'
 
 class App extends Component {
+  // Next Video is 24
+  state = {
+    todos: [
+      {id: 1, content: 'Learn React'},
+      {id: 2, content: 'Learn Django'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => 
+      {return todo.id !== id})
+    this.setState({todos})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
